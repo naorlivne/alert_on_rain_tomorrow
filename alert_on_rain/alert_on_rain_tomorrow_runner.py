163a7a0f394ed2a69b7f5e14f53c078dbf1ceb62
naorlivne <naorlivne@gmail.com>
2020-12-21 10:57:56 +0200

    adding main logic template

--- alert_on_rain/alert_on_rain_tomorrow_runner.py
+++ alert_on_rain/alert_on_rain_tomorrow_runner.py
@@ -1,5 +1,7 @@
 from parse_it import ParseIt
-import os
+from alert_on_rain.functions.weather_forecast.forecast import *
+from alert_on_rain.functions.alerts.email import *
+from alert_on_rain.functions.alerts.telegram import *
 
 
 def init():
@@ -7,4 +9,21 @@ def init():
     Run the logic which will take the variables (OWM api key & location) no matter how they are provided and will alert
     if it looks like it will rain there tomorrow
     """
-    pass
+    parser = ParseIt(recurse=False, config_type_priority=["envvars"])
+    owm_api_key = parser.read_configuration_variable("owm_api_key", required=True)
+    city = parser.read_configuration_variable("city", required=True)
+    country_code = parser.read_configuration_variable("country_code", required=True)
+    smtp_server = parser.read_configuration_variable("smtp_server", required=True)
+    sender_email = parser.read_configuration_variable("sender_email", required=True)
+    receiver_email = parser.read_configuration_variable("receiver_email", required=True)
+    email_password = parser.read_configuration_variable("email_password", required=True)
+    email_port = parser.read_configuration_variable("email_port", required=True)
+    telegram_token = parser.read_configuration_variable("telegram_token", required=True)
+    chat_id = parser.read_configuration_variable("chat_id", required=True)
+
+    owm_object = WeatherForecast(owm_api_key, city, country_code)
+    telegram_object = Telegram(telegram_token)
+
+    if owm_object.rain_tomorrow() is True:
+        telegram_object.send_alert(chat_id)
+        email_alert(smtp_server, sender_email, receiver_email, email_password, email_port)
---
+++ test/test_alert_on_rain_tomorrow_runner.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from alert_on_rain.alert_on_rain_tomorrow_runner import *
+from parse_it import ParseIt
+
+
+class BaseTests(TestCase):
+
+    def test_alert_on_rain_tomorrow_init_variables_set_correctly(self):
+        # TODO - finish the test
+        pass
+
+    def test_alert_on_rain_tomorrow_init_objects_created_correctly(self):
+        # TODO - finish the test
+        pass
+
+    def test_alert_on_rain_tomorrow_init_alert_on_rain(self):
+        # TODO - finish the test
+        pass
+
+    def test_alert_on_rain_tomorrow_init_dont_alert_on_no_rain(self):
+        # TODO - finish the test
+        pass
